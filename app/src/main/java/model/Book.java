37349d800f9467659b3ba361dd7762e3d35e5726
yh79.kang <yh79.kang@partner.samsung.com>
2016-01-07 14:27:27 +0900

    sglite  added

---
+++ app/src/main/java/model/Book.java
@@ -0,0 +1,50 @@
+package model;
+
+/**
+ * Created by yonghak on 2016-01-07.
+ */
+public class Book {
+
+    private int id;
+    private String title;
+    private String author;
+
+    public Book() {
+    }
+
+    public Book(String title, String author) {
+        super();
+        this.title = title;
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book [id=" + id + ", title=" + title + ", author=" + author
+                + "]";
+    }
+}
